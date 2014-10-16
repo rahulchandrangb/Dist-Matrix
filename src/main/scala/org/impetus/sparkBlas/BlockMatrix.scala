cd454ce7fd85b0e6f3022c9d4585126116597966
Rahul Chandran <rahul845glndsmx@gmail.com>
2014-10-16 12:45:17 +0530

    row and col cartesian update

--- src/main/scala/org/impetus/sparkBlas/BlockMatrix.scala
+++ src/main/scala/org/impetus/sparkBlas/BlockMatrix.scala
@@ -12,6 +12,7 @@ case class Block(
   val rowDiv: Int,
   val colDiv: Int,
   val data: DenseMatrix[Double]) extends Serializable {
+
   def transpose: Block = {
     val newData = data.t
     new Block(colIdx, rowIdx, colDiv, rowDiv, newData)
@@ -24,10 +25,13 @@ case class Block(
   override def toString: String = {
     data.toString
   }
-  
+
 }
 
 case class BlockMatrix(val data: RDD[Block], val rowBlockSize: Int, val colBlockSize: Int) extends Serializable {
+
+  private lazy val rowBlocksNum = data.filter(_.colIdx == 0).count.toInt
+  private lazy val colBlocksNum = data.filter(_.rowIdx == 0).count.toInt
 
   override def toString: String = {
     println("Using toString?? Use with caution..")
@@ -44,16 +48,40 @@ case class BlockMatrix(val data: RDD[Block], val rowBlockSize: Int, val colBlock
   }
 
   def multiply(otherMat: BlockMatrix) = {
-    val rowIndexed = data.map{
+    val thisRowBlockSize = data.context.broadcast(rowBlocksNum)
+    val thisColBlockSize = data.context.broadcast(colBlocksNum)
+    val otherColBlockSize = data.context.broadcast(otherMat.colBlockSize)
+
+    val rowIndexed = data.flatMap {
       r =>
-        (r.rowIdx,r)
+        val nSplitNum = otherColBlockSize.value
+        val kSplitNum = thisColBlockSize.value
+        
+        val array = Array.ofDim[((Int, Int, Int), Block)](nSplitNum)
+
+        for (i <- 0 until nSplitNum) {
+          val seq = r.rowIdx * nSplitNum * kSplitNum + i * kSplitNum + r.colIdx
+          array(i) = ((r.rowIdx, i, seq), r)
+        }
+        array
     }
-    val colIndexed = otherMat.data.map{
-      c => 
-        (c.colIdx,c)
+
+    val colIndexed = otherMat.data.map {
+      c =>
+        val mSplitNum = thisRowBlockSize.value
+        val kSplitNum = thisColBlockSize.value
+        val nSplitNum = otherColBlockSize.value
+
+        val array = Array.ofDim[((Int, Int, Int), Block)](mSplitNum)
+
+        for (j <- 0 until mSplitNum) {
+          val seq = j * nSplitNum * kSplitNum + c.colIdx * kSplitNum + c.colIdx
+          array(j) = ((j, c.colIdx, seq), c)
+        }
+        array
     }
     
     
-    
+
   }
 }
